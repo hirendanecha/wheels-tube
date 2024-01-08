a3e93e2f2b8f121718e09e3b2cddeee068caaef4
prashant <ppo.opash@gmail.com>
2024-01-08 18:50:49 +0530

    Add edit pop up on edit post

--- src/app/layouts/main-layout/pages/home/home.component.ts
+++ src/app/layouts/main-layout/pages/home/home.component.ts
@@ -28,6 +28,7 @@ import { AddFreedomPageComponent } from '../freedom-page/add-page-modal/add-page
 import { Meta } from '@angular/platform-browser';
 import { isPlatformBrowser } from '@angular/common';
 import { Howl } from 'howler';
+import { EditPostModalComponent } from 'src/app/@shared/modals/edit-post-modal/edit-post-modal.component';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -329,7 +330,7 @@ export class HomeComponent implements OnInit, AfterViewInit, OnDestroy {
       this.postData?.imageUrl ||
       this.postData?.pdfUrl
     ) {
-      if (!this.postData.meta.metalink) {
+      if (!(this.postData?.meta?.metalink || this.postData?.metalink)) {
         this.postData.metalink = null;
         this.postData.title = null;
         this.postData.metaimage = null;
@@ -382,20 +383,23 @@ export class HomeComponent implements OnInit, AfterViewInit, OnDestroy {
     // console.log('edit-post', post)
     if (post.posttype === 'V') {
       this.openUploadVideoModal(post);
-    } else if (post.pdfUrl) {
-      this.pdfName = post.pdfUrl.split('/')[3];
-      console.log(this.pdfName);
-      this.postData = { ...post };
-      this.postMessageInputValue = this.postData?.postdescription;
-    } else {
-      this.postData = { ...post };
-      this.postMessageInputValue = this.postData?.postdescription;
+    } 
+    //  else if (post.pdfUrl) {
+    //   this.pdfName = post.pdfUrl.split('/')[3];
+    //   console.log(this.pdfName);
+    //   this.postData = { ...post };
+    //   this.postMessageInputValue = this.postData?.postdescription;
+    // }
+    else {
+      this.openUploadEditPostModal(post);
+      // this.postData = { ...post };
+      // this.postMessageInputValue = this.postData?.postdescription;
     }
-    window.scroll({
-      top: 0,
-      left: 0,
-      behavior: 'smooth',
-    });
+    // window.scroll({
+    //   top: 0,
+    //   left: 0,
+    //   behavior: 'smooth',
+    // });
   }
 
   editCommunity(data): void {
@@ -559,6 +563,23 @@ export class HomeComponent implements OnInit, AfterViewInit, OnDestroy {
     modalRef.result.then((res) => {
       if (res === 'success') {
         this.socketService.socket?.on('new-post');
+      }
+    });
+  }
+  openUploadEditPostModal(post: any = {}): void {
+    const modalRef = this.modalService.open(EditPostModalComponent, {
+      centered: true, backdrop: 'static',
+    });
+    modalRef.componentInstance.title = `Edit Post`;
+    modalRef.componentInstance.confirmButtonLabel = `Save`
+    modalRef.componentInstance.cancelButtonLabel = 'Cancel';
+    modalRef.componentInstance.communityId = this.communityDetails?.Id;
+    modalRef.componentInstance.data = post.id ? post : null;
+    modalRef.result.then((res) => {
+      if (res.id) {
+        this.postData = res
+        console.log(this.postData)
+        this.uploadPostFileAndCreatePost();
       }
     });
   }
